faa666d5e6b18c8c6fd9ff754bc70234846e188f
Maxime Abade <maximeabade@gmail.com>
2022-11-17 08:41:15 +0100

    Delete abademaxim-tp2/doc directory

--- abademaxim-tp2/doc/html/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tp2_2ec_7',['tp2.c',['../tp2_8c.html',1,'']]]
-];
--- abademaxim-tp2/doc/html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['diviserentiers_9',['DiviserEntiers',['../tp2_8c.html#aa813e79a5885eecf2d0003b9ab6d5601',1,'tp2.c']]]
-];
