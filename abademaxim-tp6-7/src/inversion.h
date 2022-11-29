1a1affe7ff676fa27fa063b9bd5726ce2fbdfc4c
maximeabade <maximeabade@gmail.com>
2022-11-29 08:26:41 +0100

    edit fonction inversion

---
+++ abademaxim-tp6-7/src/inversion.h
@@ -0,0 +1,27 @@
+/*!
+    *@file inversion.h
+    *@version 1.0
+    *@author Maxime ABADE <abademaxim@cy-tech.fr>
+    *@date 15/11/2022
+    *@brief fichier header contenant les fonctions permettant le calcul d'une valeur approchée de racine de deux (~1.41)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <math.h>
+
+
+
+
+
+/*!
+    *@fn inversion
+    *@brief inversion d'un tableau (1er devient dernier etc)
+    *@param n le nombre d'itération du calcul
+    *@version 1.0
+    *@date 29/11/2022
+    *@return le tableau si tout s'est bien passé
+*/
+
