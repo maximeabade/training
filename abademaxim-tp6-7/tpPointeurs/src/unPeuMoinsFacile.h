0ee6e596c3a5eaee9365ab1f988f29058aeba027
maximeabade <maximeabade@gmail.com>
2022-12-02 08:39:39 +0100

    cleaned it all

--- abademaxim-tp6-7/tpPointeurs/src/unPeuMoinsFacile.h
+++ abademaxim-tp6-7/tpPointeurs/src/unPeuMoinsFacile.h
@@ -1,10 +1,28 @@
-# define TAILLE 5
+
+
+// Fonction pour créer un tableau à une dimension
+int* creerTabEntier1D(int int_n){
+  // Déclaration des variables
+  int* pint_tab; //tableau d'entiers
+
+  // On alloue la mémoire pour le tableau
+  pint_tab = malloc(int_n*sizeof(int));
+
+  // On vérifie que la mémoire a bien été allouée
+  if(pint_tab == NULL){
+    fprintf(stderr, "problème d'alloc\n");
+    exit(1);
+  }
+}
 
 int unPeuMoinsFacile(void){
-    int t[TAILLE] = {1, 2, 0, 1, 0};    
-    for (int *p = t; p < t + TAILLE; p++) {
+    
+
+    printf("Quelle taille pour le tableau?");
+    scanf("%d", &int_n);    
+    for (int *p = t; p < t + taille; p++) {
         if (*p == 0) {
-            printf("%ld ", t + TAILLE - p);
+            printf("%ld ", t + taille - p);
         }
     }
     printf("sont les indices des éléments nuls du tableau t\n");
