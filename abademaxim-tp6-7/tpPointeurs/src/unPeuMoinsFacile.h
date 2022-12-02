44eb2c41335dfd5e7043eb34672e0f9a87777ecf
maximeabade <maximeabade@gmail.com>
2022-12-02 09:29:06 +0100

    hbhgkgkh

--- abademaxim-tp6-7/tpPointeurs/src/unPeuMoinsFacile.h
+++ abademaxim-tp6-7/tpPointeurs/src/unPeuMoinsFacile.h
@@ -1,7 +1,9 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 // Fonction pour créer un tableau à une dimension
-int* creerTabEntier1D(int int_n){
+int* creerTabEntier1D(int int_n)
+{
   // Déclaration des variables
   int* pint_tab; //tableau d'entiers
 
@@ -13,20 +15,39 @@ int* creerTabEntier1D(int int_n){
     fprintf(stderr, "problème d'alloc\n");
     exit(1);
   }
+
+  return(pint_tab);
 }
 
-int unPeuMoinsFacile(void){
-    
+int* saisieTabEntier1D(int* pint_tab, int int_n)
+{
+  // Déclaration des variables
+  int int_i; //compteur
 
-    printf("Quelle taille pour le tableau?");
-    scanf("%d", &int_n);    
-    for (int *p = t; p < t + taille; p++) {
-        if (*p == 0) {
-            printf("%ld ", t + taille - p);
-        }
-    }
-    printf("sont les indices des éléments nuls du tableau t\n");
-    return 0;
+  // On demande à l'utilisateur de saisir les valeurs du tableau
+  for(int_i=0; int_i<int_n; int_i++){
+    printf("Saisir la valeur %d : ", int_i);
+    scanf("%d", &pint_tab[int_i]);
+  }
+  return(pint_tab);
+}
+
+
+
+int unPeuMoinsFacile(void){
+  int n;
+  
+  printf("Quelle taille pour le tableau d'entiers?");
+  scanf("%d", &n);   
+  int* t = saisieTabEntier1D(creerTabEntier1D(n), n);
+ 
+  for (int *p = t; p < t + n; p++) {
+      if (*p == 0) {
+          printf("%ld ", p - t);
+      }
+  }
+  printf("sont les indices des éléments nuls du tableau t\n");
+  return 0;
 }
 
 
