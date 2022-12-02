e93ace51e4bc174dbcc683622bd46fdfef871fcb
maximeabade <maximeabade@gmail.com>
2022-12-02 08:11:54 +0100

    lalalalala

--- abademaxim-tp6-7/tpPointeurs/src/unPeuMoinsFacile.h
+++ abademaxim-tp6-7/tpPointeurs/src/unPeuMoinsFacile.h
@@ -1,5 +1,14 @@
 # define TAILLE 5
 
-void unPeuMoinsFacile(void){
-    printf("c'est pas facile\n");
+int unPeuMoinsFacile(void){
+    int t[TAILLE] = {1, 2, 0, 1, 0};    
+    for (int *p = t; p < t + TAILLE; p++) {
+        if (*p == 0) {
+            printf("%ld ", t + TAILLE - p);
+        }
+    }
+    printf("sont les indices des éléments nuls du tableau t\n");
+    return 0;
 }
+
+
